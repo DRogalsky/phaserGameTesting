7090a6eb5676783197ee37d78cefc2b6993d81ea
DRogalsky <drogalsky14@my.whitworth.edu>
2020-08-18 18:40:45 -0700

    added bombs and score

--- scripts/game.js
+++ scripts/game.js
@@ -21,6 +21,9 @@ var platforms;
 var player;
 var cursors;
 var stars;
+var bombs;
+var score = 0;
+var scoreText;
 
 var game= new Phaser.Game(config);
 
@@ -37,8 +40,10 @@ function preload ()
 
 function create () 
 {
+    // Background
     this.add.image(400, 300, 'sky');
 
+    // Platforms
     platforms = this.physics.add.staticGroup();
 
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
@@ -47,11 +52,19 @@ function create ()
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
+    // Player
     player = this.physics.add.sprite(100, 450, 'dude');
     
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
+    // Bombs
+    bombs = this.physics.add.group();
+
+    // Score
+    scoreText = this.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#000'})
+
+    // Animations
     this.anims.create({
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude', {start: 0, end: 3}),
@@ -72,6 +85,7 @@ function create ()
         repeat: -1
     })
 
+    // stars
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
@@ -82,11 +96,15 @@ function create ()
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     })
 
+    // Colliders
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(bombs, platforms);
+    this.physics.add.collider(player, bombs, hitBomb, null, this)
 
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
+    // Controls
     cursors = this.input.keyboard.createCursorKeys();
 }
 
@@ -116,5 +134,36 @@ function update ()
 }
 
 function collectStar(player, star) {
+    // logic for when a player hits a star
     star.disableBody(true, true);
+
+    score += 10;
+    scoreText.setText('Score: ' + score)
+
+    // when they collect all stars release a bomb and make more
+    if (stars.countActive(true) === 0) {
+        stars.children.iterate(function(child) {
+
+            child.enableBody(true, child.x, 0, true, true);
+
+        })
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400,600) :Phaser.Math.Between(0,400);
+
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20)
+    }
+}
+
+function hitBomb(player, bomb) {
+    // logic for when a player hits a bomb
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+    
+    player.anims.play('turn');
+
+    gameOver = true
 }
